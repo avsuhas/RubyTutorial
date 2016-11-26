9c6d028491270ad46cc4d4033f02eab9f84d9748
Suhas Arudi <sarudi@umassd.edu>
2016-11-26 21:12:42 +0000

    inintal commit

---
+++ hello.rb
@@ -0,0 +1,23 @@
+#simple program
+
+#name = "cliff"
+
+#puts "Hello, #{name}!"
+
+#puts "Hello," + name + "!" 
+
+#method version
+
+#def hello
+#    puts "Hello, World!"
+#end
+
+#hello
+
+def hello(name)
+    puts "Hello, #{name}."
+end
+
+user_input = gets
+
+hello user_input
---
+++ problem2.rb
@@ -0,0 +1,32 @@
+# def problem(fname,lname)
+#     puts "first_name:, #{fname}."
+#     puts "last_name:, #{lname}."
+# end
+
+# first_input = gets.chomp
+# last_input = gets.chomp
+
+# problem(first_input, last_input)
+
+# name_length = (first_input + last_input).length
+
+
+
+# def join(first_input, last_input, name_length)
+#     puts "Hello, #{first_input} #{last_input}"
+#     puts "lenght of the name is #{name_length}."
+# end
+
+# join(first_input, last_input, name_length)
+
+#cliff code
+def name_length(name)
+    name.length
+end
+
+puts "whats youor name?"
+user_input =gets.chomp
+
+puts "your name is #{name_length user_input} characters long"
+puts "your name times 2 is #{name_length user_input * 2}characters long"
+        
